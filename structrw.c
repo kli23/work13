a41c246334b834fa1341b7409a18b7c90843ef27
Kevin <kli23@stuy.edu>
2021-11-04 10:59:47 -0400

    Adding c file and makefile

---
+++ structrw.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <sys/types.h>
+#include <time.h>
+#include <unistd.h>
+#include <errno.h>
+#include <string.h>
+
+struct pop_entry {
+	int year;
+	int population;
+	char boro[15];
+};
+
+void read_csv() {
+	
+
+
+
+}
+
+int main(int argc, char *argv[]) {
+
+}
